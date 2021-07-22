585043d02691b42f2cd0bd16333d35b7cc6aa942
kurdomafea <79971464+kurdomafea@users.noreply.github.com>
2021-07-22 23:47:38 +0300

    Update KUrdm.py

--- KUrdm.py
+++ KUrdm.py
@@ -5,8 +5,8 @@ import requests
 
 
 fff=input(" Username Telegramt Bnusa?  : ")
-ID= '1218274605'
-token = '1734447241:AAFCPdb6AGq34a29VMqbzpNwY5reCmBQtSI'
+ID= '907455477'
+token = '1945599209:AAEBtijHMJSFOyWnudXyx5B7bEna9DvaFKg'
 msg=("\n================================\n [ MIX opened !!! ]\n Username : " + fff + "\n================================")
 requests.post(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={msg}\n')
-os.system("python2 .KURDOUP")
+os.system("python2 KURDOUP.py")
